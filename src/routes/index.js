0acb39a95be2bb5a3563cae054bd7df709d5c9be
ytkonaft <boho.ufo@gmail.com>
2019-04-30 00:00:23 +0300

    init project

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  // res.render("index", { title: "Express" });
+  res.json({ message: "azazaz" });
+});
+
+router.post("/login", (req, res) => {
+  res.json({ message: "azazaz" });
+});
+
+export default router;
